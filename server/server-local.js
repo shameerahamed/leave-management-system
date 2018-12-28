b61ed51145702da508488e8b7c6e98efa63777be
Shameer Ahamed <shameerahamed.f@gmail.com>
2018-12-28 20:58:19 +0800

    For Netlify deploy

---
+++ server/server-local.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import app from './server'
+
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import mongoose from './src/config/mongoose';
 import cors from 'cors';
+import serverless from 'serverless-http';
 
 import schema from './src/graphql';
 
 const db = mongoose();
 const app = express();
+const router = express.Router();
 
 app.use('*', cors());
 
@@ -15,8 +17,11 @@ app.use('/graphql', cors(), graphqlHTTP({
   rootValue: global,
   graphiql: true
 }));
-
+app.use('/.netlify/functions/server', router);
 // Up and Running at Port 4000
-app.listen(process.env.PORT || 8080, () => {
+/*app.listen(process.env.PORT || 8080, () => {
   console.log('A GraphQL API running at port 4000');
-});
+});*/
+
+module.exports = app;
+module.exports.handler = serverless(app);
